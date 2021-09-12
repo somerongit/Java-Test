bda158ddcfacdbf949299405037855d1f92a8840
somerongit <someron.bakuli05@gmail.com>
2021-09-12 15:46:49 +0530

    New Update!

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 class Test { 
    public static void  main(String []arg){
-        System.out.print("Hi! I am Som 3.0");
+        double temp=509.509;
+        System.out.println("Hi! I am Som 3.0");
+        System.out.println("Output is: "+(int)temp);
    }
 }
