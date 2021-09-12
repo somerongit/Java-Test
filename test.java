07d1cef65a4c0511ee01561c62b0a6fa3f91ec65
somerongit <someron.bakuli05@gmail.com>
2021-09-12 20:07:08 +0530

    Obj,Method using initObj

--- test.java
+++ test.java
@@ -1,12 +1,23 @@
 class Test { 
-   public void eat(){
-         System.out.println("I am Eadting");
+
+        String name;
+        int age;
+   public void initObj(String passName, int passAge){
+         name=passName;
+         age=passAge;
+         System.out.println("Object is Initialized!");
+   }
+   public void display(){
+
+        System.out.println("Display is Called!");
+        System.out.println("Name is: "+name+"\nAge is: "+age);
    }
    public static void  main(String []arg){
 
         System.out.println("Hi! I am Som 3.0");
 
         Test obj=new Test();
-        obj.eat();
+        obj.initObj("Oke",25);
+        obj.display();
    }
 }
