5fe991faae7240386849f72eadb264ba5dd24796
somerongit <someron.bakuli05@gmail.com>
2021-09-12 21:34:38 +0530

    Simple Inheritence

--- test.java
+++ test.java
@@ -1,10 +1,10 @@
-class Test { 
+class Online { 
 
         String name;
         int age;
-   public void initObj(String passName, int passAge){
-         name=passName;
-         age=passAge;
+   public Online(String passName, int passAge){
+         this.name=passName;
+         this.age=passAge;
          System.out.println("Object is Initialized!");
    }
    public void display(){
@@ -12,12 +12,22 @@ class Test {
         System.out.println("Display is Called!");
         System.out.println("Name is: "+name+"\nAge is: "+age);
    }
-   public static void  main(String []arg){
+
+}
+
+class Test extends Online{
+   public Test(String passName, int passAge){
+         super(passName,passAge);
+         name=passName;
+         age=passAge;
+         System.out.println("Object is Initialized!");
+   }
+
+   public static void  main(String[] arg){
 
         System.out.println("Hi! I am Som 3.0");
 
-        Test obj=new Test();
-        obj.initObj("Oke",25);
+        Test obj=new Test("Oke",25);
         obj.display();
    }
 }
