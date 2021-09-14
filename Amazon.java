c51a4b4f3ef21adb085809a4c77d0018214f9ef6
somerongit <someron.bakuli05@gmail.com>
2021-09-14 09:31:16 +0530

    Method Define inside Functional Interface, Functional Interface with 2 methods

--- Amazon.java
+++ Amazon.java
@@ -1,6 +1,15 @@
 @FunctionalInterface
 interface Abc{
     void display();
+/*
+    public void initAbc(){System.out.println("Init Abc");}
+
+    We can't define a method inside a Interface untill 1.7, but after that
+    we can define a method inside a interface by only using "default"
+    because every method inside the Interface is by default added "abstract"
+    keyword which can not be defined, so we need to use this keyword as below
+*/
+    default void initAbc(){System.out.println("Define Method Inside Interface!");}
 }
 
 
@@ -16,6 +25,7 @@ class Amazon{
    After using Lamda Expression which is came from Scale
 */
           Abc obj = () -> System.out.println("Implementing Functional Intreface using Lamda Expression");
+          obj.initAbc();
           obj.display();
 
        }
