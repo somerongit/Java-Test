2cb5750782e7111d9b04ce15a462cd5d25061fda
somerongit <someron.bakuli05@gmail.com>
2021-09-14 09:45:39 +0530

    Or : Multiple Implements with common function by overrideing methods using Super

--- Amazon.java
+++ Amazon.java
@@ -1,11 +1,11 @@
 @FunctionalInterface
 interface Abc{
     void display();
-    default void initAbc(){System.out.println("Define Method Inside Interface!");}
+    default void initAbc(){System.out.println("Define Method Inside Abc Interface!");}
 }
 
 interface Abc2{
-    default void initAbc(){System.out.println("Define Method Inside Interface!");}
+    default void initAbc(){System.out.println("Define Method Inside Abc2 Interface!");}
 }
 
 class Amazon implements Abc,Abc2{
@@ -13,7 +13,10 @@ class Amazon implements Abc,Abc2{
       System.out.println("Interface is Implemeted");
       }
       public void initAbc(){
-      System.out.println("Multiple Implements with common function by overrideing methods");
+
+      Abc2.super.initAbc();
+
+//      System.out.println("Multiple Implements with common function by overrideing methods");
       }
       public static void main(String args[]){
           Amazon obj = new Amazon();
