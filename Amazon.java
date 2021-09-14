ff62c5dc3fa9c1ace21069dfd33e2c54e30f1f42
somerongit <someron.bakuli05@gmail.com>
2021-09-14 12:38:13 +0530

    Static block execution simulation

--- Amazon.java
+++ Amazon.java
@@ -1,16 +1,19 @@
-@FunctionalInterface
-interface Abc{
-    void display();
-    int Age=25;// It is a constant final
-// Interface can have a static method too
-    static void initAbc(){System.out.println("Define Static Method Inside Abc Interface!");}
-}
-
 class Amazon{
-      public static void main(String args[]){
-          
-          Abc.initAbc();
-
+    static{
+         int age=25;
+         String lname="Bakuli";
+         System.out.println("1st Static Block Excecuting...");
+       }
+    Amazon(){
+         System.out.println("Constructor Excecuting...");
+      }
+    static{
+         String fname="Someron";
+         System.out.println("2nd Static Block Excecuting...");
+       }
+    public static void main(String args[]){
+          System.out.println("Main Method is executing...");
+          Amazon obj=new Amazon();
        }
 
 }
